a64d4acc97204bc7deaa4aa2527737006427ce45
Emanuel Caesar <emanuel.caesar@gmail.com>
2018-06-14 10:25:22 +0200

    Updated 3 class ready to use

--- src/main/java/model/Music.java
+++ src/main/java/model/Music.java
@@ -1,4 +1,47 @@
 package model;
 
 public class Music {
+    private String genre;
+    private String format;
+    private String year;
+    private String Artist;
+
+    public Music(String genre, String format, String year, String artist) {
+        this.genre = genre;
+        this.format = format;
+        this.year = year;
+        Artist = artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getArtist() {
+        return Artist;
+    }
+
+    public void setArtist(String artist) {
+        Artist = artist;
+    }
 }
