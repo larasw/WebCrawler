2c6ab4a71f6dcc661a9a90b04b1507dc31a7fb0e
Laras Widiastuti <laraswidias008@gmail.com>
2018-06-14 10:56:19 +0200

    Crawl_Result Test - Constructor To Instantiate Books ArrayList Test

--- src/main/java/result/Crawl_Result.java
+++ src/main/java/result/Crawl_Result.java
@@ -1,4 +1,35 @@
 package result;
 
+import model.Book;
+import model.Movie;
+import model.Music;
+
+import java.util.ArrayList;
+
 public class Crawl_Result {
+    private int Id, numberOfPages;
+    private String timeElapsed;
+    private ArrayList<Book> books;
+    private ArrayList<Movie> movies;
+    private ArrayList<Music> music;
+
+    public Crawl_Result() {
+        books = new ArrayList<>();
+    }
+
+    public void AddBook(Book book){
+
+    }
+
+    public void AddMovie(Movie movie){
+
+    }
+
+    public void AddMusic(Music music){
+
+    }
+
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
 }
--- src/test/java/result/Crawl_ResultTest.java
+++ src/test/java/result/Crawl_ResultTest.java
@@ -1,7 +1,20 @@
 package result;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
 
 public class Crawl_ResultTest {
+    @Test
+    public void constructorToInstantiateBooksArrayListTest() {
+        // arrange
+        Crawl_Result result = new Crawl_Result();
 
+        // act
+        int total = result.getBooks().size();
+
+        // assert
+        assertThat(total).isEqualTo(0);
+    }
 }
