b2014b7c4ed78021e795bd6b71bbf5469a9f053e
Emanuel Caesar <emanuel.caesar@gmail.com>
2018-06-17 18:23:28 +0200

    TestConstructorMovie

---
+++ src/test/java/model/BookTest.java
@@ -0,0 +1,56 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookTest {
+
+    @Test
+    public void getGenre() {
+    }
+
+    @Test
+    public void setGenre() {
+    }
+
+    @Test
+    public void getFormat() {
+    }
+
+    @Test
+    public void setFormat() {
+    }
+
+    @Test
+    public void getYear() {
+    }
+
+    @Test
+    public void setYear() {
+    }
+
+    @Test
+    public void getAuthors() {
+    }
+
+    @Test
+    public void setAuthors() {
+    }
+
+    @Test
+    public void getPublisher() {
+    }
+
+    @Test
+    public void setPublisher() {
+    }
+
+    @Test
+    public void getISBN() {
+    }
+
+    @Test
+    public void setISBN() {
+    }
+}
