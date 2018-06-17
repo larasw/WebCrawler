7b90c120af6804d90dd72cc197ee25ed10508cbc
Feter Akira Vedaalana Sitepu,Feter Akira <f.vedaalana@student.fontys.nl>
2018-06-17 19:54:26 +0200

    Adding DFS Class

---
+++ src/main/java/strategy/DFS.java
@@ -0,0 +1,16 @@
+package strategy;
+
+public class DFS {
+
+    public void getPageLinks(String URL) {
+
+    }
+
+    public void extractor(String search){
+
+    }
+
+    public void jsonResult(){
+        
+    }
+}
