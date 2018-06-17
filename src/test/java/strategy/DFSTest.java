dda1293421cd3835e9524c38fef0374674fcc8ca
Feter Akira Vedaalana Sitepu,Feter Akira <f.vedaalana@student.fontys.nl>
2018-06-17 19:55:37 +0200

    Adding DFS Test

---
+++ src/test/java/strategy/DFSTest.java
@@ -0,0 +1,20 @@
+package strategy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DFSTest {
+
+    @Test
+    public void getPageLinks() {
+    }
+
+    @Test
+    public void extractor() {
+    }
+
+    @Test
+    public void jsonResult() {
+    }
+}
