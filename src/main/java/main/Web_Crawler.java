713b2b358a3ce388ed49e5577cd6b03b5ea317a9
Feter Akira Vedaalana Sitepu,Feter Akira <f.vedaalana@student.fontys.nl>
2018-06-18 02:47:36 +0200

    fixing Istrategy

--- src/main/java/main/Web_Crawler.java
+++ src/main/java/main/Web_Crawler.java
@@ -1,5 +1,6 @@
 package main;
 
+import org.json.JSONException;
 import strategy.DFS;
 
 import java.util.Scanner;
@@ -8,12 +9,12 @@ public class Web_Crawler {
     // The purpose of this comment is to check Jenkins Automated Test
     // Which is really important for our final project :)
     // Thank you
-    public static void main(String[] args){
+    public static void main(String[] args) {
         DFS bwc = new DFS();
         bwc.getPageLinks("http://localhost/sample_site_to_crawl/");
         Scanner s = new Scanner(System.in);
         String a = s.next();
-        bwc.extractor(a);
+        bwc.Extractor(a);
         bwc.jsonResult();
     }
 }
